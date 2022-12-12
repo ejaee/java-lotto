950e5f4aaf8c06ffe623c169950ce653991eed79
choiejae <ejae8319@gmail.com>
2022-12-12 23:37:44 +0900

    feat: LottoTicket 의 일급컬렉션인 LottoTickets 생성

--- src/main/java/lotto/domain/lottoticket/LottoTickets.java
+++ src/main/java/lotto/domain/lottoticket/LottoTickets.java
@@ -1,5 +1,30 @@
 package lotto.domain.lottoticket;
 
+import java.util.List;
+import java.util.Objects;
+
 public class LottoTickets {
+    private final List<LottoTicket> lottoTickets;
+
+    private LottoTickets(List<LottoTicket> lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+    public static LottoTickets from(List<LottoTicket> lottoTickets) {
+        return new LottoTickets(lottoTickets);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final LottoTickets that = (LottoTickets) o;
+        return Objects.equals(lottoTickets, that.lottoTickets);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoTickets);
+    }
 
 }
---
+++ src/test/java/lotto/domain/lottoticket/LottoTicketsTest.java
@@ -0,0 +1,40 @@
+package lotto.domain.lottoticket;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LottoTicketsTest {
+
+    List<LottoTicket> lottoTicket;
+
+    @BeforeEach
+    void setUp() {
+        // given
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        lottoTicket = Arrays.asList(new LottoTicket(lottoNumbers), new LottoTicket(lottoNumbers), new LottoTicket(lottoNumbers));
+    }
+
+    @Test
+    void create() {
+        // when
+        LottoTickets lottoTickets = LottoTickets.from(lottoTicket);
+
+        // then
+        assertThat(lottoTickets).isNotNull();
+    }
+
+    @Test
+    void equals() {
+        // when
+        LottoTickets lottoTickets = LottoTickets.from(lottoTicket);
+
+        // then
+        assertThat(lottoTickets).isEqualTo(LottoTickets.from(lottoTicket));
+    }
+
+}
